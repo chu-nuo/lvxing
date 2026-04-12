ca26033f664e2caaca2e7e4e8c86c954d769e497
罗美琪 <luomeiqi1126@163.com>
2026-04-12 12:38:44 +0800

    fix: 使用 ES 模块语法重写 api/index.js

--- api/index.js
+++ api/index.js
@@ -3,14 +3,13 @@
  * 用于 Express 应用的 Serverless 包装
  */
 
-// 使用 require() 而不是 ES6 import，以兼容 Vercel Serverless Functions
-const { createApp } = require('../server/dist/app.js');
+import { createApp } from '../server/dist/app.js';
 
 // 创建 Express 应用
 const app = createApp();
 
 // Vercel Serverless Function handler
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   // Vercel 的 rewrite 规则将 /api/* 转发到这里
   // 但 Express 内部路由期望不带 /api 前缀
   // 所以需要去掉 /api 前缀
@@ -20,4 +19,4 @@ module.exports = async function handler(req, res) {
     req.url = '/';
   }
   return app(req, res);
-};
+}
